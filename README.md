d552da1a7b1113cbadefc2fe46f7331095ea034a
Victor Augusto <victoraweb@gmail.com>
2018-06-30 19:47:02 -0300

    [FEAT] Add basic publishing

---
+++ README.md
@@ -0,0 +1,9 @@
+# python-rabbitmq-lab
+
+Running RabbitMQ with docker:
+`docker pull rabbitmq:3-management-alpine`
+
+`docker run --name rabbit-with-management -p 5672:5672 -p 15672:15672 rabbitmq:3-management-alpine`
+
+`5672`: Port to connect to rabbimq
+`15672`: Port to management. `localhost:15672`. username and password: `guest`
---
+++ src/receive.py
@@ -0,0 +1,20 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print(f'Message received: {body}')
+
+channel.basic_consume(
+    callback,
+    queue='hello',
+    no_ack=True
+)
+
+print('Listening for messages...')
+
+channel.start_consuming()
